1c6f9ac506fe96d6a70f228962fa2f4b6bb102d1
Chong Liu <erinyues.us@gmail.com>
2014-09-30 16:30:39 -0400

    latest adt version & google drive version

--- RoboCatProject(from google drive)/gen/com/robocatpackage/BuildConfig.java
+++ RoboCatProject(from google drive)/gen/com/robocatpackage/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.robocat.roboappui;
+package com.robocatpackage;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
---
+++ RoboCatProject(from google drive)/src/com/robocatpackage/PololuMaestroDevice.java
@@ -0,0 +1,15 @@
+package com.robocatpackage;
+
+import android.hardware.usb.UsbDevice;
+
+/**
+ * Interface for abstracting Device type (USB, Serial, etc.).
+ *
+ * 
+ */
+public interface PololuMaestroDevice {
+	int readInt();
+	void sendCommand(byte[] buffer, int length);
+	public void setDevice(UsbDevice device);
+}
+
