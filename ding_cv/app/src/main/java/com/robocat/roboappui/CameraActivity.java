8bf4179859b822e5f61c4a1b6181c7824a78b2fa
Pinfang Ding <dingpinfang@gmail.com>
2014-12-09 16:00:39 -0500

    untest face func

---
+++ ding_cv/app/src/main/java/com/robocat/roboappui/CameraActivity.java
@@ -0,0 +1,112 @@
+package com.robocat.roboappui;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.hardware.Camera.CameraInfo;
+
+public class CameraActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    private String TAG = "Camera Activity";
+
+    /**
+     * Initializes the Camera and CameraPreview
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        try{
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+        catch (Exception e){
+            Log.e(TAG, "error in creation");
+        }
+        Button button1 = (Button) findViewById(R.id.button_capture);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                releaseMediaRecorder();
+                releaseCamera();
+                finish();
+            }
+        });
+
+    }
+
+    /** A safe way to get an instance of the Camera object.
+     *  @return c - Camera instance or null
+     */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(CameraInfo.CAMERA_FACING_FRONT); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * Releases the Camera and MediaRecorder
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    /**
+     * Releases the Camera and MediaRecorder
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    /**
+     * Not implemented but applicable for
+     * future implementation of a MediaRecorder
+     */
+    private void releaseMediaRecorder(){
+//        if (mMediaRecorder != null) {
+//            mMediaRecorder.reset();   // clear recorder configuration
+//            mMediaRecorder.release(); // release the recorder object
+//            mMediaRecorder = null;
+//            mCamera.lock();           // lock camera for later use
+//        }
+    }
+
+    /**
+     * Releases the Camera
+     */
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+}
